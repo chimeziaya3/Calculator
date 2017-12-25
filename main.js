69a7651f3ab0f07f8cb51b1439e9ceb4fba89c75
Chimezie Anyaoha <Chim@Chimezies-MacBook-Pro.local>
2017-12-24 21:53:04 -0500

    added average function

--- main.js
+++ main.js
@@ -20,3 +20,15 @@ function div(){
 function result(){
 	document.getElementById('display').value=eval(document.getElementById('display').value);
 }
+// My average function. Can't seem to get it to work
+function average(value){
+	var sum=0;
+	var numbers=value.split("+");
+	for (let i = 0; i < numbers.length; i++) {
+        sum=sum+numbers[i];
+        
+    }
+	output=sum/numbers.length;
+    
+	document.getElementById('display').value=output;
+}
